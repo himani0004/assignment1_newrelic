c561579cc1e647544d859aeb00db5ab52760ee04
himani0004 <himanisharma0004@gmail.com>
2023-08-04 12:33:11 +0530

    first

---
+++ my-awesome-nerdpack/nerdlets/home/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class SimpleCounter extends Component {
+  state = {
+    count: 0,
+  };
+//increment the count by 1
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+  //decrement the count by 1
+  decrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+  render() {
+    const { count } = this.state;
+    return (
+      <div className='main'>
+        <h1>Simple Counter</h1>
+        <p>Count: {count}</p>
+        <div className = "xyz">
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>decrement</button>
+      </div>
+      </div>
+    );
+  }
+}
+
